2fadd8af64f478d12060cd7a2ca39ff86e15fa23
crabtwtw <y921921@gmail.com>
2026-03-30 06:43:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+
+TG_TOKEN = "8728915298:AAF9WW6ddW_TTxpwEHa_hBfzaV8Qs-emy6g"
+CHAT_ID = "5494623381"
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
 
 @app.route('/')
 def home():
@@ -8,9 +20,19 @@ def home():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    data = request.json or {}
+
+    msg = f"""
+📢 TradingView訊號
+幣種：{data.get("symbol")}
+方向：{data.get("signal")}
+價格：{data.get("price")}
+"""
+
+    send(msg)
+
     return {"ok": True}
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
